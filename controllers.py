9493e9fd4bdc61e81d637e7e0701c46ac8a3df91
bruno+assuncao <bruno.assuno@rocketmail.com>
2020-05-26 15:05:31 -0300

    Incluido /nomedointegrante e /presenca

--- controllers.py
+++ controllers.py
@@ -5,6 +5,7 @@ from flask import render_template
 from flask import redirect
 from flask import request
 from models import Post
+#from models import Presenca (após criar no models.py)   
 
 @app.route('/post/gravar', methods=['POST']) #recebe mensagem do formulario
 
@@ -47,6 +48,9 @@ def index():
     menu.append({'active': False,
                 'href': '/gledson',
                 'texto': 'Sobre - Gledson'})
+    menu.append({'active': False,
+                'href': '/presenca',
+                'texto': 'Formulário - Presença'})
     ## Inserimos tudo que foi criado no dicionário context, ele será passado para a view
     context = {'titulo': 'Página principal',
             'menu': menu,
@@ -80,6 +84,9 @@ def post():
     menu.append({'active': False,
                 'href': '/gledson',
                 'texto': 'Sobre - Gledson'})
+    menu.append({'active': False,
+                'href': '/presencan',
+                'texto': 'Formulário - Presença'})
     
     context = {'titulo': 'Escrever post', #muda frase na tela e tiitulo página
             'menu': menu}
@@ -111,6 +118,9 @@ def priscila():
     menu.append({'active': False,
                 'href': '/gledson',
                 'texto': 'Sobre - Gledson'})
+    menu.append({'active': False,
+                'href': '/presenca',
+                'texto': 'Formulário - Presença'})
     
     context = {'titulo': 'Sobre - Priscila', #muda frase na tela e tiitulo página
             'menu': menu}
@@ -142,6 +152,9 @@ def anderson():
     menu.append({'active': False,
                 'href': '/gledson',
                 'texto': 'Sobre - Gledson'})
+    menu.append({'active': False,
+                'href': '/presenca',
+                'texto': 'Formulário - Presença'})
     
     context = {'titulo': 'Sobre - Priscila', #muda frase na tela e tiitulo página
             'menu': menu}
@@ -173,6 +186,9 @@ def bruno():
     menu.append({'active': False,
                 'href': '/gledson',
                 'texto': 'Sobre - Gledson'})
+    menu.append({'active': False,
+                'href': '/presenca',
+                'texto': 'Formulário - Presença'})
     
     context = {'titulo': 'Sobre - Bruno', #muda frase na tela e tiitulo página
             'menu': menu}
@@ -204,6 +220,9 @@ def mateus():
     menu.append({'active': False,
                 'href': '/gledson',
                 'texto': 'Sobre - Gledson'})
+    menu.append({'active': False,
+                'href': '/presenca',
+                'texto': 'Formulário - Presença'})
     
     context = {'titulo': 'Sobre - Mateus', #muda frase na tela e tiitulo página
             'menu': menu}
@@ -235,9 +254,46 @@ def gledson():
     menu.append({'active': True,
                 'href': '/gledson',
                 'texto': 'Sobre - Gledson'})
+    menu.append({'active': False,
+                'href': '/presenca',
+                'texto': 'Formulário - Presença'})
     
     context = {'titulo': 'Sobre - Gledson', #muda frase na tela e tiitulo página
             'menu': menu}
     return render_template('gledson.html', **context)
+
+@app.route('/presenca')
+def presenca():
+    menu = []
+    
+    ## Cada opção no menu é um dicionário
+    menu.append({'active': False, # active informa se a opção está ativa, e se estiver, destaca ela na página
+                'href': '/', # href é o caminho que deve ser aberto pela opção
+                'texto': 'Página principal'}) # texto é o texto exibido no menu para a opção
+    menu.append({'active': False,
+                'href': '/post',
+                'texto': 'Escrever post'})
+    menu.append({'active': False,
+                'href': '/priscila',
+                'texto': 'Sobre - Priscila'})
+    menu.append({'active': False,
+                'href': '/anderson',
+                'texto': 'Sobre - Anderson'})
+    menu.append({'active': False,
+                'href': '/bruno',
+                'texto': 'Sobre - Bruno'})
+    menu.append({'active': False,
+                'href': '/mateus',
+                'texto': 'Sobre - Mateus'})
+    menu.append({'active': False,
+                'href': '/gledson',
+                'texto': 'Sobre - Gledson'})
+    menu.append({'active': True,
+                'href': '/presenca',
+                'texto': 'Formulário - Presença'})
+    
+    context = {'titulo': 'Formulário - Presença', #muda frase na tela e tiitulo página
+            'menu': menu}
+    return render_template('presenca.html', **context)
 
 app.run()
