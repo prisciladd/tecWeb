f74492f561df968984d68d9eddf4a0dc00ca7379
Anderson da Silva Machado <anderson361163@gmail.com>
2020-05-27 19:29:06 -0300

    Alterações de persistência
    *Modificação da tabela posts;
    *Modificação do model (responsável pela lógica de persistência).

--- models.py
+++ models.py
@@ -1,33 +1,30 @@
+## models.py
 from banco import bd
 
+
 class Post:
-    def __init__(self, titulo,autor, texto):
-        self.titulo= titulo
-        self.autor= autor
-        self.texto= texto
+    def __init__(self, titulo, autor, texto):
+        self.titulo = titulo
+        self.autor = autor
+        self.texto = texto
 
-    def gravar (self):
+    def gravar(self):
         sql = '''insert into posts (titulo, autor, texto) values (?, ?, ?)'''
-        primeiro_interrogacao= self.titulo
-        segundo_interrogacao= self.autor
-        terceiro_interrogacao= self.texto
-        
+        primeiro_interrogacao = self.titulo
+        segundo_interrogacao = self.autor
+        terceiro_interrogacao = self.texto
         bd().execute(sql, [primeiro_interrogacao, segundo_interrogacao, terceiro_interrogacao])
-
         bd().commit()
-        
+
     @staticmethod
     def recupera_todos():
+        ## Usamos o objeto retornado por bd() para realizar comandos sql
         sql = '''select titulo, autor, texto from posts order by id desc'''
-        cur= bd().execute(sql)
-        ## Montamos dicionários com os resultados da consulta para passar para a view
+        cur = bd().execute(sql)
+        ## Montamos dicionário dicionários com os resultados da consulta para passar para a view
         posts = []
         for titulo, autor, texto in cur.fetchall(): # fetchall() gera uma lista com os resultados:
-            post = Post (titulo,autor, texto)
+            post = Post(titulo, autor, texto)
             posts.append(post)
-
-        return posts 
-
-
-    
-
+        
+        return posts
