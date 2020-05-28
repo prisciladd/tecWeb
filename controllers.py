36bebf4dc918fac4b7517ce10a7b1e7fa9a13784
Anderson da Silva Machado <anderson361163@gmail.com>
2020-05-27 22:06:32 -0300

    Resolvi o problema de gravação de dados
    Mudança no nome da tabela e conexão do formulário com o banco

--- controllers.py
+++ controllers.py
@@ -4,14 +4,24 @@ from aplicacao import app
 from flask import render_template
 from flask import redirect
 from flask import request
-from models import Post
+from models import Presente
+#from models import Presenca (após criar no models.py)   
 
+@app.route('/presenca/gravar', methods=['POST'])
+def gravar_presenca():
+    presencaa = request.form['presenca']
+    pergunta = request.form['pergunta']
+    email = request.form['email']
+    texto = request.form['texto']
+    veioHoje = Presente(presencaa, pergunta, email, texto)
+    veioHoje.gravar()
+    return redirect('/')
 
 @app.route('/')
 def index():
-    # mensagens = Mensagem.recupera_todas()
-    posts = Post.recupera_todos()
-
+    dados = Presente.recupera_todos()
+    ## Usamos o objeto retornado por bd() para realizar comandos sql
+    
     ## Insere opções no menu
     menu = []
     ## Cada opção no menu é um dicionário
@@ -19,66 +29,214 @@ def index():
                 'href': '/', # href é o caminho que deve ser aberto pela opção
                 'texto': 'Página principal'}) # texto é o texto exibido no menu para a opção
     menu.append({'active': False,
-                'href': '/post',
-                'texto': 'Escrever post'})
+                'href': '/priscila',
+                'texto': 'Sobre - Priscila'})
     menu.append({'active': False,
-                'href': '/fabio',
-                'texto': 'Sobre - Fabio'})
-
+                'href': '/anderson',
+                'texto': 'Sobre - Anderson'})
+    menu.append({'active': False,
+                'href': '/bruno',
+                'texto': 'Sobre - Bruno'})
+    menu.append({'active': False,
+                'href': '/mateus',
+                'texto': 'Sobre - Mateus'})
+    menu.append({'active': False,
+                'href': '/gledson',
+                'texto': 'Sobre - Gledson'})
+    menu.append({'active': False,
+                'href': '/presenca',
+                'texto': 'Formulário - Presença'})
     ## Inserimos tudo que foi criado no dicionário context, ele será passado para a view
     context = {'titulo': 'Página principal',
             'menu': menu,
-            'posts': posts}
-
+            'posts': dados
+            }
     return render_template('index.html', **context)
 
-@app.route('/post')
-def post():
+@app.route('/priscila')
+def priscila():
     menu = []
+    
     ## Cada opção no menu é um dicionário
     menu.append({'active': False, # active informa se a opção está ativa, e se estiver, destaca ela na página
                 'href': '/', # href é o caminho que deve ser aberto pela opção
                 'texto': 'Página principal'}) # texto é o texto exibido no menu para a opção
     menu.append({'active': True,
-                'href': '/post',
-                'texto': 'Escrever post'})
+                'href': '/priscila',
+                'texto': 'Sobre - Priscila'})
     menu.append({'active': False,
-                'href': '/fabio',
-                'texto': 'Sobre - Fabio'})
-
-    context = {'titulo': 'Escrever post',
+                'href': '/anderson',
+                'texto': 'Sobre - Anderson'})
+    menu.append({'active': False,
+                'href': '/bruno',
+                'texto': 'Sobre - Bruno'})
+    menu.append({'active': False,
+                'href': '/mateus',
+                'texto': 'Sobre - Mateus'})
+    menu.append({'active': False,
+                'href': '/gledson',
+                'texto': 'Sobre - Gledson'})
+    menu.append({'active': False,
+                'href': '/presenca',
+                'texto': 'Formulário - Presença'})
+    
+    context = {'titulo': 'Sobre - Priscila', #muda frase na tela e tiitulo página
             'menu': menu}
+    return render_template('priscila.html', **context)
 
-    return render_template('post.html', **context)
-
-
-@app.route('/post/gravar', methods=['POST'])
-def gravar_post():
-    titulo = request.form['titulo']
-    autor = request.form['autor']
-    texto = request.form['texto']
-    post = Post(titulo, autor, texto)
-    post.gravar()
-    return redirect('/')
-
-@app.route('/fabio')
-def fabio():
+@app.route('/anderson')
+def anderson():
     menu = []
+    
     ## Cada opção no menu é um dicionário
     menu.append({'active': False, # active informa se a opção está ativa, e se estiver, destaca ela na página
                 'href': '/', # href é o caminho que deve ser aberto pela opção
                 'texto': 'Página principal'}) # texto é o texto exibido no menu para a opção
     menu.append({'active': False,
-                'href': '/post',
-                'texto': 'Escrever post'})
+                'href': '/priscila',
+                'texto': 'Sobre - Priscila'})
     menu.append({'active': True,
-                'href': '/fabio',
-                'texto': 'Sobre - Fabio'})
-
-    context = {'titulo': 'Sobre - Fabio',
+                'href': '/anderson',
+                'texto': 'Sobre - Anderson'})
+    menu.append({'active': False,
+                'href': '/bruno',
+                'texto': 'Sobre - Bruno'})
+    menu.append({'active': False,
+                'href': '/mateus',
+                'texto': 'Sobre - Mateus'})
+    menu.append({'active': False,
+                'href': '/gledson',
+                'texto': 'Sobre - Gledson'})
+    menu.append({'active': False,
+                'href': '/presenca',
+                'texto': 'Formulário - Presença'})
+    
+    context = {'titulo': 'Sobre - Priscila', #muda frase na tela e tiitulo página
             'menu': menu}
+    return render_template('anderson.html', **context)
 
-    return render_template('fabio.html', **context)
+@app.route('/bruno')
+def bruno():
+    menu = []
+    
+    ## Cada opção no menu é um dicionário
+    menu.append({'active': False, # active informa se a opção está ativa, e se estiver, destaca ela na página
+                'href': '/', # href é o caminho que deve ser aberto pela opção
+                'texto': 'Página principal'}) # texto é o texto exibido no menu para a opção
+    menu.append({'active': False,
+                'href': '/priscila',
+                'texto': 'Sobre - Priscila'})
+    menu.append({'active': False,
+                'href': '/anderson',
+                'texto': 'Sobre - Anderson'})
+    menu.append({'active': True,
+                'href': '/bruno',
+                'texto': 'Sobre - Bruno'})
+    menu.append({'active': False,
+                'href': '/mateus',
+                'texto': 'Sobre - Mateus'})
+    menu.append({'active': False,
+                'href': '/gledson',
+                'texto': 'Sobre - Gledson'})
+    menu.append({'active': False,
+                'href': '/presenca',
+                'texto': 'Formulário - Presença'})
+    
+    context = {'titulo': 'Sobre - Bruno', #muda frase na tela e tiitulo página
+            'menu': menu}
+    return render_template('bruno.html', **context)
 
+@app.route('/mateus')
+def mateus():
+    menu = []
+    
+    ## Cada opção no menu é um dicionário
+    menu.append({'active': False, # active informa se a opção está ativa, e se estiver, destaca ela na página
+                'href': '/', # href é o caminho que deve ser aberto pela opção
+                'texto': 'Página principal'}) # texto é o texto exibido no menu para a opção
+    menu.append({'active': False,
+                'href': '/priscila',
+                'texto': 'Sobre - Priscila'})
+    menu.append({'active': False,
+                'href': '/anderson',
+                'texto': 'Sobre - Anderson'})
+    menu.append({'active': False,
+                'href': '/bruno',
+                'texto': 'Sobre - Bruno'})
+    menu.append({'active': True,
+                'href': '/mateus',
+                'texto': 'Sobre - Mateus'})
+    menu.append({'active': False,
+                'href': '/gledson',
+                'texto': 'Sobre - Gledson'})
+    menu.append({'active': False,
+                'href': '/presenca',
+                'texto': 'Formulário - Presença'})
+    
+    context = {'titulo': 'Sobre - Mateus', #muda frase na tela e tiitulo página
+            'menu': menu}
+    return render_template('mateus.html', **context)
+
+@app.route('/gledson')
+def gledson():
+    menu = []
+    
+    ## Cada opção no menu é um dicionário
+    menu.append({'active': False, # active informa se a opção está ativa, e se estiver, destaca ela na página
+                'href': '/', # href é o caminho que deve ser aberto pela opção
+                'texto': 'Página principal'}) # texto é o texto exibido no menu para a opção
+    menu.append({'active': False,
+                'href': '/priscila',
+                'texto': 'Sobre - Priscila'})
+    menu.append({'active': False,
+                'href': '/anderson',
+                'texto': 'Sobre - Anderson'})
+    menu.append({'active': False,
+                'href': '/bruno',
+                'texto': 'Sobre - Bruno'})
+    menu.append({'active': False,
+                'href': '/mateus',
+                'texto': 'Sobre - Mateus'})
+    menu.append({'active': True,
+                'href': '/gledson',
+                'texto': 'Sobre - Gledson'})
+    menu.append({'active': False,
+                'href': '/presenca',
+                'texto': 'Formulário - Presença'})
+    
+    context = {'titulo': 'Sobre - Gledson', #muda frase na tela e tiitulo página
+            'menu': menu}
+    return render_template('gledson.html', **context)
+
+@app.route('/presenca')
+def presenca():
+    menu = []
+    
+    ## Cada opção no menu é um dicionário
+    menu.append({'active': False, # active informa se a opção está ativa, e se estiver, destaca ela na página
+                'href': '/', # href é o caminho que deve ser aberto pela opção
+                'texto': 'Página principal'}) # texto é o texto exibido no menu para a opção
+    menu.append({'active': False,
+                'href': '/priscila',
+                'texto': 'Sobre - Priscila'})
+    menu.append({'active': False,
+                'href': '/anderson',
+                'texto': 'Sobre - Anderson'})
+    menu.append({'active': False,
+                'href': '/bruno',
+                'texto': 'Sobre - Bruno'})
+    menu.append({'active': False,
+                'href': '/mateus',
+                'texto': 'Sobre - Mateus'})
+    menu.append({'active': False,
+                'href': '/gledson',
+                'texto': 'Sobre - Gledson'})
+    menu.append({'active': True,
+                'href': '/presenca',
+                'texto': 'Formulário - Presença'})
+    
+    context = {'titulo': 'Formulário - Presença', #muda frase na tela e tiitulo página
+            'menu': menu}
+    return render_template('presenca.html', **context)
 
 app.run()
--- models.py
+++ models.py
@@ -1,30 +1,35 @@
-## models.py
 from banco import bd
 
+class Presente:
+    def __init__(self, presenca,pergunta, email, texto):
+        self.presenca= presenca
+        self.pergunta= pergunta
+        self.email= email
+        self.texto= texto
 
-class Post:
-    def __init__(self, titulo, autor, texto):
-        self.titulo = titulo
-        self.autor = autor
-        self.texto = texto
+    def gravar (self):
+        sql = '''insert into presente (presenca, pergunta, email, texto) values (?, ?, ?, ?)'''
+        primeiro_interrogacao= self.presenca
+        segundo_interrogacao= self.pergunta
+        terceiro_interrogacao= self.email
+        quarta_interrogacao= self.texto
 
-    def gravar(self):
-        sql = '''insert into posts (titulo, autor, texto) values (?, ?, ?)'''
-        primeiro_interrogacao = self.titulo
-        segundo_interrogacao = self.autor
-        terceiro_interrogacao = self.texto
-        bd().execute(sql, [primeiro_interrogacao, segundo_interrogacao, terceiro_interrogacao])
+        bd().execute(sql, [primeiro_interrogacao, segundo_interrogacao, terceiro_interrogacao, quarta_interrogacao])
+
         bd().commit()
-
+        
     @staticmethod
     def recupera_todos():
-        ## Usamos o objeto retornado por bd() para realizar comandos sql
-        sql = '''select titulo, autor, texto from posts order by id desc'''
-        cur = bd().execute(sql)
-        ## Montamos dicionário dicionários com os resultados da consulta para passar para a view
-        posts = []
-        for titulo, autor, texto in cur.fetchall(): # fetchall() gera uma lista com os resultados:
-            post = Post(titulo, autor, texto)
-            posts.append(post)
-        
-        return posts
+        sql = '''select presenca, pergunta, email, texto from presente order by id desc'''
+        cur= bd().execute(sql)
+        ## Montamos dicionários com os resultados da consulta para passar para a view
+        informações = []
+        for presenca, pergunta, email, texto in cur.fetchall(): # fetchall() gera uma lista com os resultados:
+            dados = Presente(presenca,pergunta, email, texto)
+            informações.append(dados)
+
+        return informações 
+
+
+    
+
