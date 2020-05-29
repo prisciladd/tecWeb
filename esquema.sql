63ae08144336bc0a2673a0717162bcd5b6d994f6
Anderson da Silva Machado <anderson361163@gmail.com>
2020-05-28 21:56:42 -0300

    Mudança do nome da tabela
    Alterando o nome da tabela de posts para presente

--- esquema.sql
+++ esquema.sql
@@ -1,8 +1,8 @@
 -- esquema.sql
 -- Toda definição de banco deverá ser feita nesse arquivo
 
-drop table if exists posts;
-create table posts (
+drop table if exists presente;
+create table presente (
   id integer primary key autoincrement,
   titulo string not null,
   autor string not null,
