a679b09baed3c41ad867778d6690a37727586435
Priscila Da Dalt <priscila.dalt@aluno.faculdadeimpacta.com.br>
2020-06-05 21:35:20 -0300

    add tabela presenca no esquema sql

--- esquema.sql
+++ esquema.sql
@@ -1,10 +1,18 @@
 -- esquema.sql
 -- Toda definição de banco deverá ser feita nesse arquivo
 
-drop table if exists presente;
-create table presente (
+drop table if exists posts;
+create table posts (
   id integer primary key autoincrement,
   titulo string not null,
   autor string not null,
   texto string not null
+);
+
+drop table if exists presenca;
+create table presenca (
+  email string not null,
+  presenca string not null,
+  resposta string not null,
+  comentarios string not null
 );
